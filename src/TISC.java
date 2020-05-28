4c208672b9bfb15a6f0a75e14c9c98c305b86539
Edfame <theedfame@gmail.com>
2020-05-28 19:34:33 +0100

    Added comments.

--- src/TISC.java
+++ src/TISC.java
@@ -73,6 +73,10 @@ public class TISC {
     public void storeArg(int distancia, int numero) {
     }
 
+    /**
+     * Retira os dois elementos no topo da pilha
+     * e empilha a soma dos dois.
+     */
     public void add() {
 
         int direita = pilhaDeAvaliacao.pop();
@@ -81,6 +85,10 @@ public class TISC {
         pilhaDeAvaliacao.push(direita + esquerda);
     }
 
+    /**
+     * Retira os dois elementos no topo da pilha
+     * e empilha a divisão dos dois.
+     */
     public void div() {
 
         int direita = pilhaDeAvaliacao.pop();
@@ -89,14 +97,22 @@ public class TISC {
         pilhaDeAvaliacao.push(esquerda / direita);
     }
 
+    /**
+     * Retira os dois elementos no topo da pilha
+     * e efetua a operação de pontenciação.
+     */
     public void exp() {
 
-        int direita = pilhaDeAvaliacao.pop();
-        int esquerda = pilhaDeAvaliacao.pop();
+        int expoente = pilhaDeAvaliacao.pop();
+        int base = pilhaDeAvaliacao.pop();
 
-        pilhaDeAvaliacao.push((int) Math.pow(esquerda, direita));
+        pilhaDeAvaliacao.push((int) Math.pow(base, expoente));
     }
 
+    /**
+     * Retira os dois elementos no topo da pilha
+     * e empilha o módulo da divisão dos dois.
+     */
     public void mod() {
 
         int direita = pilhaDeAvaliacao.pop();
@@ -105,6 +121,10 @@ public class TISC {
         pilhaDeAvaliacao.push(esquerda % direita);
     }
 
+    /**
+     * Retira os dois elementos no topo da pilha
+     * e empilha a multiplicação dos dois.
+     */
     public void mult() {
 
         int direita = pilhaDeAvaliacao.pop();
@@ -113,6 +133,10 @@ public class TISC {
         pilhaDeAvaliacao.push(direita * esquerda);
     }
 
+    /**
+     * Retira os dois elementos no topo da pilha
+     * e empilha a subtração dos dois.
+     */
     public void sub() {
 
         int direita = pilhaDeAvaliacao.pop();
@@ -127,32 +151,60 @@ public class TISC {
     public void locals(int argumentos, int variaveis) {
     }
 
+    /**
+     * Faz o environment pointer apontar para o Registo de Ativação anterior .
+     */
     public void returnInst() {
+
+        evp = memoriaDeExecucao.get(evp);
+
     }
 
     public void setArg(int numero) {
     }
 
+    /**
+     * Empilha o inteiro dado no topo da pilha.
+     *
+     * @param inteiro número a ser empilhado.
+     */
     public void pushInt(int inteiro) {
 
         pilhaDeAvaliacao.push(inteiro);
     }
 
+    /**
+     * Coloca no standart out o valor que está no topo da pilha.
+     */
     public void print() {
 
         System.out.print(pilhaDeAvaliacao.pop());
     }
 
+    /**
+     * Coloca no standart out "\n".
+     */
     public void printNl() {
 
         System.out.print("\n");
     }
 
+    /**
+     * Coloca no standart out a string passada.
+     *
+     * @param string valor que é imprimido.
+     */
     public void printString(String string) {
 
         System.out.print(string);
     }
 
+    /**
+     * Salta para a etiqueta caso os valores no
+     * topo da pilha sejam iguais.
+     *
+     * @param etiqueta aponta para a instrução para onde saltar.
+     */
     public void jeq(Etiqueta etiqueta) {
 
         int a = pilhaDeAvaliacao.pop();
@@ -165,6 +217,12 @@ public class TISC {
         }
     }
 
+    /**
+     * Seja A o valor no topo da pilha e B o seguinte,
+     * salta para a etiqueta caso B < A.
+     *
+     * @param etiqueta aponta para a instrução para onde saltar.
+     */
     public void jlt(Etiqueta etiqueta) {
 
         int a = pilhaDeAvaliacao.pop();
@@ -177,6 +235,11 @@ public class TISC {
         }
     }
 
+    /**
+     * Salta para a etiqueta.
+     *
+     * @param etiqueta aponta para a instrução para onde saltar.
+     */
     public void jump(Etiqueta etiqueta) {
 
         pc = etiquetas.get(etiqueta);
